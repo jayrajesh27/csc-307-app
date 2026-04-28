38bdeb68231defa49797198b4e650291c1d61f73
Jay K Rajesh <jrajesh@calpoly.edu>
2026-04-28 12:10:31 -0700

    wire backend to mongoose, add db queries for filter and delete

--- packages/express-backend/backend.js
+++ packages/express-backend/backend.js
@@ -1,65 +1,24 @@
 // backend.js
 import express from "express";
+import cors from "cors";
+import dotenv from "dotenv";
+import mongoose from "mongoose";
+import userService from "./services/user-service.js";
+
+dotenv.config();
+
+const { MONGO_CONNECTION_STRING } = process.env;
+
+mongoose.set("debug", true);
+mongoose
+  .connect(MONGO_CONNECTION_STRING + "users")
+  .catch((error) => console.log(error));
 
 const app = express();
 const port = 8000;
 
+app.use(cors());
 app.use(express.json());
-
-const users = {
-  users_list: [
-    {
-      id: "xyz789",
-      name: "Charlie",
-      job: "Janitor",
-    },
-    {
-      id: "abc123",
-      name: "Mac",
-      job: "Bouncer",
-    },
-    {
-      id: "ppp222",
-      name: "Mac",
-      job: "Professor",
-    },
-    {
-      id: "yat999",
-      name: "Dee",
-      job: "Aspring actress",
-    },
-    {
-      id: "zap555",
-      name: "Dennis",
-      job: "Bartender",
-    },
-  ],
-};
-
-// ---- helper functions ----
-
-const findUserByName = (name) => {
-  return users["users_list"].filter(
-    (user) => user["name"] === name
-  );
-};
-
-const findUserById = (id) =>
-  users["users_list"].find((user) => user["id"] === id);
-
-// for step 7 - find by both name and job
-const findUserByNameAndJob = (name, job) => {
-  return users["users_list"].filter(
-    (user) => user["name"] === name && user["job"] === job
-  );
-};
-
-const addUser = (user) => {
-  users["users_list"].push(user);
-  return user;
-};
-
-// ---- routes ----
 
 app.get("/", (req, res) => {
   res.send("Hello World!");
@@ -68,49 +27,58 @@ app.get("/", (req, res) => {
 app.get("/users", (req, res) => {
   const name = req.query.name;
   const job = req.query.job;
-
-  // if both name and job are provided, filter by both
-  if (name != undefined && job != undefined) {
-    let result = findUserByNameAndJob(name, job);
-    result = { users_list: result };
-    res.send(result);
-  } else if (name != undefined) {
-    let result = findUserByName(name);
-    result = { users_list: result };
-    res.send(result);
-  } else {
-    res.send(users);
-  }
+  userService
+    .getUsers(name, job)
+    .then((result) => res.send({ users_list: result }))
+    .catch((error) => {
+      console.log(error);
+      res.status(500).send("internal server error");
+    });
 });
 
 app.get("/users/:id", (req, res) => {
   const id = req.params["id"];
-  let result = findUserById(id);
-  if (result === undefined) {
-    res.status(404).send("Resource not found.");
-  } else {
-    res.send(result);
-  }
+  userService
+    .findUserById(id)
+    .then((result) => {
+      if (result === null) {
+        res.status(404).send("Resource not found.");
+      } else {
+        res.send(result);
+      }
+    })
+    .catch((error) => {
+      console.log(error);
+      res.status(404).send("Resource not found.");
+    });
 });
 
 app.post("/users", (req, res) => {
   const userToAdd = req.body;
-  addUser(userToAdd);
-  res.status(201).send(userToAdd);
+  userService
+    .addUser(userToAdd)
+    .then((newUser) => res.status(201).send(newUser))
+    .catch((error) => {
+      console.log(error);
+      res.status(500).send("could not add user");
+    });
 });
 
-// step 7 - delete a user by id
 app.delete("/users/:id", (req, res) => {
   const id = req.params["id"];
-  const index = users["users_list"].findIndex(
-    (user) => user["id"] === id
-  );
-  if (index === -1) {
-    res.status(404).send("User not found.");
-  } else {
-    users["users_list"].splice(index, 1);
-    res.status(204).send();
-  }
+  userService
+    .deleteUserById(id)
+    .then((result) => {
+      if (result === null) {
+        res.status(404).send("User not found.");
+      } else {
+        res.status(204).send();
+      }
+    })
+    .catch((error) => {
+      console.log(error);
+      res.status(404).send("User not found.");
+    });
 });
 
 app.listen(port, () => {
